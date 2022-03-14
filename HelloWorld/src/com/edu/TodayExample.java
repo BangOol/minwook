84c470680aa7e46be924c54c22ef4c4082c5f8f7
admin <admin@YD-11>
2022-03-14 17:43:28 +0900

    final commit

--- HelloWorld/src/com/edu/TodayExample.java
+++ HelloWorld/src/com/edu/TodayExample.java
@@ -7,28 +7,51 @@ import java.util.Scanner;
 //키 입력(double) -> scn.nextDouble();
 
 // 이름은 : ???
-// 연락처 : 010-8330-2939
-// 나이 : 28
-// 키 :173.9
+// 연락처 : ??
+// 나이 : ??
+// 키 :??
+// 적정몸무게 : ?? 
 public class TodayExample {
 	public static void main(String[] args) {
 	
 		Scanner scn = new Scanner(System.in);
 		
 		int age;
-		double tall;
+		double tall, BMI, kg;
 		String name, tel;
 		
+		System.out.println("이름이 뭔가요?");
 		name = scn.nextLine();
+		System.out.println("연락처가 어떻게 되나요?");
 		tel = scn.nextLine();
+		System.out.println("나이가 어떻게 되나요?");
 		age = scn.nextInt();
+		System.out.println("키가 어떻게 되나요?");
 		tall = scn.nextDouble();
+		System.out.println("몸무게가 어떻게 되나요?");
+		kg = scn.nextDouble();
+		
 
+		BMI =(double)((tall-100)*0.9);
 		
 		System.out.println("이름은 " + name + " 입니다.");
 		System.out.println("연락처는 " + tel + " 입니다.");
 		System.out.print("나이는 " + age + "살이고, ");
 		System.out.println("키는 " + tall + "cm 입니다.");
+		System.out.println("적정몸무게는 " + BMI + "kg 입니다.");
+		
+		// BMI 이상 : 과체중
+		// BMI 적정 : 정상체중
+		// BMI 이하 : 저체중
+		
+		
+			
+		if((BMI + 5) > kg && (BMI -5) <= kg) {
+			System.out.println("현재  정상 체중입니다.");
+		} else{
+			System.out.println("현재  정상 체중이 아닙니다. 체중을 " + (kg - BMI) + "kg 빼셔야 합니다.");
+		
+		}
 	
 	}
 }
