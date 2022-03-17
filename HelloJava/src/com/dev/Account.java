9f96704795960bf542158a9707bcbcee1408987b
admin <admin@YD-11>
2022-03-17 17:51:05 +0900

    0317 commit2

--- HelloJava/src/com/dev/Account.java
+++ HelloJava/src/com/dev/Account.java
@@ -9,6 +9,9 @@ public class Account {
 	
 	
 	//constructor
+	// 생성자: 목적은 필드의 초기값을 지정하기 위함
+	// 즉, 원래라면 일일히 필드에 대한 값을 지정하기 위해 타이핑을 많이 해야 했지만
+	// 생성자를 통해 미리 그 형태를 지정함으로써 많은 양의 정보를 적은 줄로 해결할 수 있다는거 아닐가
 	
 	public Account(String accNo, String accName, int money) { //계좌번호, 예금주, 통장금액
 		super();
--- HelloJava/src/com/dev/BankApp.java
+++ HelloJava/src/com/dev/BankApp.java
@@ -1,14 +1,19 @@
 package com.dev;
 import java.util.Scanner;
 
+// ++ 파일경로와 스캐너 사용
 public class BankApp {
 	
-	// 다른 메소드에도 인식을 하기 위해서
-	static Account[] banks = new Account[100]; //main 안에서도 호출을 하기 위해서 static으로 메모리에 인식시켜야...
+	// ++ 다른 메소드에도 인식을 하기 위해서 Account와 Scanner 인스턴스를 밖에 꺼냄.
+	// ++ 그렇게 함으로써 다른 메소드에서도 인식이 되고, 사용이 가능해짐.
+	// ++ main 안에서도 호출을 하기 위해서 static으로 메모리에 인식시켜야...
+	static Account[] banks = new Account[100]; 
 	static Scanner scn = new Scanner(System.in); 
 	
 	public static void main(String[] args) {
 		
+	// ++ 반복문 while와 그 안의 if- else if 구문으로 번호에 따라서 통장개설, 인출, 출금, 금액확인, 리스트 확인을 체크하고자 함.
+	// ++ 다만 main 메소드에서 과정을 나타내고 싶지 않고, 추가로 public static void 메소드를 통해 초기값을 설정하고자 했음.
 		while(true) {
 			printMenu();
 			
@@ -37,7 +42,10 @@ public class BankApp {
 	
 	
 	public static void printMenu() {
-		// 메뉴 출력 기능
+		// ++ 메뉴 출력 기능
+		// ++ 메뉴에 들어간 글을 변수에 저장하고, 출력하는 메소드를 만들었음.
+		// ++ 생성자는 아니나, static을 사용하여 처음 컴퓨터가 메모리를 인식할 때 이 내용을 초기값으로 기억하며
+		// ++ public 으로 다른 패키지에서도 사용할 수 있도록 설정되었다.
 		String menu = "===========================================\r\n"
 				+ " 메뉴를 보여주고 선택할 수 있도록 함.\r\n"
 				+ " 1: 계좌생성(계좌번호, 예금주, 초기예금금액)"
@@ -55,7 +63,10 @@ public class BankApp {
 	
 	
 	public static void createAccount() {
-		// 통장개설
+		// ++ 통장개설
+		// ++ 스캐너 기능과 문자출력을 하고, 우리가 입력한 값들을 새로운 aacnt라는 인스턴스에 저장하고자 하였음.
+		// ++ 이 때, 우리가 밖에서 지정해놓았었던 Account[] banks 인스턴스라는 계좌들을5 저장하는 곳에 이 accnt에 기입된 내용들을 저장하고자 하였기에 for 구문을 사용
+		// ++ 
 		System.out.println("계좌생성기능");
 		System.out.print("계좌번호입력>");
 		String accNo = scn.next();
@@ -77,6 +88,32 @@ public class BankApp {
 	public static void deposit() {
 		// 예금 기능
 		System.out.println("예금기능");
+		String ano =scn.next();
+		int amt = scn.nextInt();
+		int checkCnt = 0;  // 조회가 되었는지 체크 여부 변수
+		
+		for(int i =0; i <banks.length; i++) { // ++ 계좌번호가 있을경우
+			if(banks[i] !=null && banks[i].getAccNo().equals(ano)){ // ++ 계좌번호가 조회되었을 때
+				
+				checkCnt = 1; // 실행되었단느 의미
+				int currAmt = banks[i].getMoney();
+				if(currAmt + amt > 100000) {
+					checkCnt = 2;
+					break; //++ 실행되었지만 예금액이 10만원을 넘겼을 때 초과하지못하도록
+				}
+				banks[i].setMoney(currAmt + amt); // 잔액 + 입금액
+				break;
+			}
+		}
+		
+		if(checkCnt == 1) {
+			System.out.println("정상적으로 처리되었습니다");
+		}else if(checkCnt == 2) {
+			System.out.println("한도금액을 초과했습니다.");
+		}
+		else {
+			System.out.println("처리되지 못했습니다.");
+		}
 	}
 	
 	
